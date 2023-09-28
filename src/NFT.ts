b7b77ea3826d190477afa4f142d55af7b3d11ab3
Raunaque97 <rpatra1997@gmail.com>
2023-09-28 17:49:00 +0530

    MerkleWitness error in NFT test

--- src/NFT.ts
+++ src/NFT.ts
@@ -56,13 +56,13 @@ export class NFT extends RuntimeModule<{}> {
   public transfer(to: PublicKey, key: NFTKey) {
     const nft = this.records.get(key).value;
     // update the owner to the 'to' address
-    this.records.set(
-      key,
-      new NFTEntity({ owner: to, metadata: nft.metadata, locked: Bool(false) })
-    );
+    // this.records.set(
+    //   key,
+    //   new NFTEntity({ owner: to, metadata: nft.metadata, locked: Bool(false) })
+    // );
 
-    // nft.owner = to;
-    // this.records.set(key, nft);
+    nft.owner = to;
+    this.records.set(key, nft);
   }
 
   public lock(key: NFTKey) {
